14173a6667599aa53510bd320f28a74d2f9c10a0
水野翔平 <gold.experience3260@gmail.com>
2017-07-02 20:02:44 +0900

    teach yourself java 1.8.1

---
+++ teach-yourself-java/src/foundation_1/practice_1_8_1.java
@@ -0,0 +1,9 @@
+package foundation_1;
+
+public class practice_1_8_1 {
+	public static void main(String args[]) {
+		System.out.println("This is on the first line.");
+		System.out.println("This is on the second line.");
+	}
+
+}
