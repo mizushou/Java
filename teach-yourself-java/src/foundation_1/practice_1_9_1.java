06c7d6db5192349a6853c08b5b3bb8429554db06
水野翔平 <gold.experience3260@gmail.com>
2017-07-03 17:24:34 +0900

    add practice_1_9_1

---
+++ teach-yourself-java/src/foundation_1/practice_1_9_1.java
@@ -0,0 +1,9 @@
+package foundation_1;
+
+public class practice_1_9_1 {
+	public static void main(String arg[]) {
+		int num;
+		num = 1000;
+		System.out.println(num + " is the value of num");
+	}
+}
